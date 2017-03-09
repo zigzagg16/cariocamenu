07bec4bb05d1fd00dadc33a0ef2f18f2f992e67e
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-03-09 20:37:49 +0100

    link test target

--- cariocaTests/cariocaTests.swift
+++ cariocaTests/cariocaTests.swift
@@ -2,11 +2,10 @@
 //  cariocaTests.swift
 //  cariocaTests
 //
-//  Created by Arnaud Schloune on 25/07/15.
-//  Copyright (c) 2015 Arnaud Schloune. All rights reserved.
+//  Created by Arnaud Schloune on 09/03/2017.
+//  Copyright © 2017 Arnaud Schloune. All rights reserved.
 //
 
-import UIKit
 import XCTest
 
 class cariocaTests: XCTestCase {
@@ -23,12 +22,12 @@ class cariocaTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
