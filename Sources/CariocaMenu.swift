9d2811cb62ce5f7115ca56e87039c8214fc85294
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-11-21 18:33:49 +0100

    base project, generated with SwiftPlate

---
+++ Sources/CariocaMenu.swift
@@ -0,0 +1,9 @@
+//
+//  CariocaMenu.swift
+//  CariocaMenu
+//
+//  Created by Arnaud Schloune on 21/11/2017.
+//  Copyright © 2017 CariocaMenu. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import CariocaMenuTests
+
+XCTMain([
+    testCase(CariocaMenuTests.allTests),
+])
