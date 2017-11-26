8ce7c93e46008a004777034bea56606236e64246
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-11-26 18:56:05 +0100

    added 2 more tests

--- Tests/CariocaMenuTests/CariocaGestureManagerTests.swift
+++ Tests/CariocaMenuTests/CariocaGestureManagerTests.swift
@@ -19,16 +19,28 @@ class CariocaGestureManagerTests: XCTestCase {
         let menuHeight: CGFloat = 240.0
         let heightForRow: CGFloat = 60.0
         var selectedIndex: Int = 1
-        var yRange: ClosedRange<CGFloat> = 20.0...765
+        var yRange: ClosedRange<CGFloat> = 20.0...700
         var isOffScreenAllowed: Bool = true
+        func position() -> CGFloat {
+            return CariocaGestureManager.topYConstraint(yLocation: yLocation,
+                                                 originalScreeenEdgePanY: originalScreenEdgePanY,
+                                                 menuHeight: menuHeight, heightForRow: heightForRow,
+                                                 selectedIndex: selectedIndex,
+                                                 yRange: yRange,
+                                                 isOffscreenAllowed: isOffScreenAllowed)
+        }
         //When
-        let yPosition1 = CariocaGestureManager.topYConstraint(yLocation: yLocation,
-                                                              originalScreeenEdgePanY: originalScreenEdgePanY,
-                                                              menuHeight: menuHeight, heightForRow: heightForRow,
-                                                              selectedIndex: selectedIndex,
-                                                              yRange: yRange,
-                                                              isOffscreenAllowed: true)
+        let yPosition1 = position()
+        selectedIndex = 3
+        originalScreenEdgePanY = 50
+        let yPosition2 = position()
+        originalScreenEdgePanY = 600
+        isOffScreenAllowed = false
+        let yPosition3 = position()
+        
         //Then
-        XCTAssertEqual(yPosition1, Double(614.00), accuracy: 0.001)
+        XCTAssertEqual(yPosition1, CGFloat(614.00), accuracy: 0.001)
+        XCTAssertEqual(yPosition2, CGFloat(-90.00), accuracy: 0.001)
+        XCTAssertEqual(yPosition3, CGFloat(700.00), accuracy: 0.001)
     }
 }
