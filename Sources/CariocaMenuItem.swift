7a97889ca1b38c4801516bd185fb4971134b3253
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-01 20:04:13 +0100

    ✈️ indicator view end position calculation

---
+++ Sources/CariocaMenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  CariocaMenuItem.swift
+//  CariocaMenu
+//
+//  Created by Arnaud Schloune on 01/12/2017.
+//  Copyright © 2017 CariocaMenu. All rights reserved.
+//
+
+import Foundation
+
+///Carioca Menu Item
+public class CariocaMenuItem {
+
+}
