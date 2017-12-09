59ce9525cc3e5d505c509c1d3832c6888c034c25
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-09 21:42:37 +0100

    put some gradients in background

--- CariocaMenuDemo/DemoViewController.swift
+++ CariocaMenuDemo/DemoViewController.swift
@@ -8,6 +8,11 @@ import UIKit
 class DemoViewController: UIViewController {
     var carioca: CariocaMenu?
 	@IBOutlet weak var iconView: CariocaIconView!
+	@IBOutlet weak var gradientView: ASGradientView!
+
+	override func viewDidLoad() {
+		setupGradient()
+	}
 
     override func viewDidAppear(_ animated: Bool) {
 		iconView.label.font = UIFont.boldSystemFont(ofSize: 75)
@@ -42,6 +47,25 @@ class DemoViewController: UIViewController {
 		coordinator.animateAlongsideTransition(in: self.view, animation: nil, completion: { [weak self] _ in
 			self?.carioca?.hostViewDidRotate()
 		})
+	}
+	// MARK: Gradient setup
+	func setupGradient() {
+		gradientView.colors = [
+			//turquoise, green sear
+			(start: UIColor(red: 0.17, green: 0.73, blue: 0.61, alpha: 1.00),
+			 end: UIColor(red: 0.14, green: 0.62, blue: 0.52, alpha: 1.00)),
+			//emerald, nephritis
+			(start: UIColor(red: 0.23, green: 0.79, blue: 0.45, alpha: 1.00),
+			 end: UIColor(red: 0.20, green: 0.67, blue: 0.39, alpha: 1.00)),
+			//amethyst, wisteria
+			(start: UIColor(red: 0.60, green: 0.36, blue: 0.71, alpha: 1.00),
+			 end: UIColor(red: 0.55, green: 0.29, blue: 0.67, alpha: 1.00)),
+			//sunflower, orange
+			(start: UIColor(red: 0.95, green: 0.61, blue: 0.17, alpha: 1.00),
+			 end: UIColor(red: 0.95, green: 0.61, blue: 0.17, alpha: 1.00))
+		]
+
+		gradientView.animateGradient()
 	}
 }
 
@@ -79,7 +103,7 @@ class CariocaCustomIndicatorView: UIView, CariocaIndicatorConfiguration {
 		polygonPath.addLine(to: CGPoint(x: frame.minX + 0.03423 * frame.width, y: frame.minY + 0.35204 * frame.height))
 		polygonPath.addLine(to: CGPoint(x: frame.minX + 0.21405 * frame.width, y: frame.minY + 0.10454 * frame.height))
 		polygonPath.close()
-		UIColor(red: 0.63, green: 0.76, blue: 0.22, alpha: 1.00).setFill()
+		UIColor(red:  0.63, green:  0.76, blue:  0.22, alpha:  1.00).setFill()
 		polygonPath.fill()
 		return polygonPath
 	}
