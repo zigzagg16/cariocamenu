4fc21180a36462872a434c37cb4bc0bf7a5bea83
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-14 00:21:03 +0100

    storyboardable protocol

---
+++ CariocaMenuDemo/external/Storyboardable.swift
@@ -0,0 +1,28 @@
+//
+//  Storyboardable.swift
+//
+// Original Source: https://gist.github.com/eddies5/74e878acf3ada119fa14db1851fd00fe#file-storyboardablev2-swift
+//
+
+import Foundation
+import UIKit
+
+protocol Storyboardable: class {
+	static var defaultStoryboardName: String { get }
+}
+
+extension Storyboardable where Self: UIViewController {
+	static var defaultStoryboardName: String {
+		return "Main"
+	}
+
+	static func fromStoryboard() -> Self {
+		let storyboard = UIStoryboard(name: defaultStoryboardName, bundle: nil)
+		guard let vc = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as? Self else {
+			fatalError("Could not instantiate view controller")
+		}
+		return vc
+	}
+}
+
+extension UIViewController: Storyboardable { }
