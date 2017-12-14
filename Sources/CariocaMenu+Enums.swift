06d6465fae92cfd0d21ff6d4b47e1c5813bb3ee2
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-14 20:35:07 +0100

    add horizontal indicator key

--- Sources/CariocaMenu+Enums.swift
+++ Sources/CariocaMenu+Enums.swift
@@ -12,8 +12,10 @@ import Foundation
 public enum BoomerangType: Int {
     ///The indicators will always return where they were let.
     case none = 0
-    ///The indicators will always come back at the same Y value. They may switch from Edge if the user wants.
-    case vertical = 1
+	///The indicator will return always on the original edge.
+	case horizontal = 1
+    ///The indicators will always come back at the same Y value. It may switch from Edge if the user wants.
+    case vertical = 2
     ///The indicators will always come back at the exact same place
-    case verticalHorizontal = 2
+    case verticalHorizontal = 3
 }
