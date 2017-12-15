eb8795c266b7aeafa4171b0799ad7681ec0ae5e2
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-15 14:21:43 +0100

    add helpers in required demo controllers

--- CariocaMenuDemo/DemoControllers/DemoIdeaViewController.swift
+++ CariocaMenuDemo/DemoControllers/DemoIdeaViewController.swift
@@ -5,4 +5,8 @@ class DemoIdeaViewController: UIViewController, DemoController {
 	var menuController: CariocaController?
 
 	var gradientColors: [(start: UIColor, end: UIColor)] = []
+
+	override func viewWillAppear(_ animated: Bool) {
+		self.view.addCariocaGestureHelpers([.left, .right])
+	}
 }
