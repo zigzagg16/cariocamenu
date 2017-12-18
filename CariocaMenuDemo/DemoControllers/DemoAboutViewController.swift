c3c02e98177b735ef9536d4709a233121c67a36b
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-12-18 14:09:48 +0100

    about view

--- CariocaMenuDemo/DemoControllers/DemoAboutViewController.swift
+++ CariocaMenuDemo/DemoControllers/DemoAboutViewController.swift
@@ -5,4 +5,17 @@ class DemoAboutViewController: UIViewController, DemoController {
 	var menuController: CariocaController?
 
 	var gradientColors: [(start: UIColor, end: UIColor)] = []
+
+	@IBAction func actionTwitter(_ sender: UIButton) {
+		url("https://twitter.com/mmommommomo")
+	}
+
+	@IBAction func actionGithub(_ sender: UIButton) {
+		url("https://github.com/arn00s/")
+	}
+
+	func url(_ urlString: String) {
+		guard let url = URL(string: urlString) else { print("Could not open \(urlString)");return }
+		UIApplication.shared.openURL(url)
+	}
 }
